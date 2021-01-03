95327b313546cadc660c277083e31e2c4e9f4df9
gojkovicmatija99 <gojkovicmatija99@gmail.com>
2021-01-03 20:56:00 +0100

    Kreiran view za servis 2

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,85 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+class Flight {
+  constructor(id, airplaneId, startDestination, endDestination, distance, price, currentPassengers, isCanceled) {
+    this.id = id;
+    this.airplaneId = airplaneId;
+    this.startDestination = startDestination;
+    this.endDestination = endDestination;
+    this.distance = distance;
+    this.price = price;
+    this.currentPassengers = currentPassengers;
+    if(isCanceled)
+      this._rowVariant = "danger" 
+  }
+}
+
 export default new Vuex.Store({
   state: {
+    flights:[]
   },
   mutations: {
+    set_flights: function (state, flightsJson) {
+      state.flights = []
+      for(var index in flightsJson) {
+        var id = flightsJson[index].id;
+        var airplaneId = flightsJson[index].airplaneId;
+        var startDestination = flightsJson[index].startDestination;
+        var endDestination = flightsJson[index].endDestination;
+        var distance = flightsJson[index].distance;
+        var price = flightsJson[index].price;
+        var currentPassengers = flightsJson[index].currentPassengers;
+        var canceled = flightsJson[index].canceled;
+        const flight = new Flight(id, airplaneId, startDestination, endDestination, distance, price, currentPassengers, canceled);
+        state.flights.push(flight);
+      }
+    }
   },
   actions: {
+    load_available_flights: function ({ commit }, page) {
+      fetch('http://localhost:8082/flight/all/' + page, { method: 'get' }).then((response) => {
+        if (!response.ok)
+          throw response;
+        return response.json()
+      }).then((flightsJson) => {
+        commit('set_flights', flightsJson)
+      }).catch((error) => {
+        if (typeof error.text === 'function')
+          error.text().then((errorMessage) => {
+            alert(errorMessage);
+          });
+        else
+          alert(error);
+      });
+    },
+    load_filtered_flights: function ({ commit }, dto) {
+      var name = dto[0]
+      var obj = {};
+      obj[name] = dto[1]
+      const filter = JSON.stringify(obj);
+      console.log(filter);
+      fetch('http://localhost:8082/flight/search', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: filter
+      }).then((response) => {
+        if (!response.ok)
+          throw response;
+        return response.json()
+      }).then((flightsJson) => {
+        commit('set_flights', flightsJson)
+      }).catch((error) => {
+        if (typeof error.text === 'function')
+          error.text().then((errorMessage) => {
+            alert(errorMessage);
+          });
+        else
+          alert(error);
+      });
+    },
   },
   modules: {
   }
---
+++ src/views/Flights.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="flights">
+    <b-container>    
+      <h1>All available flights</h1><br>  
+      <b-row>
+        <b-col col lg="7">
+          <b-pagination
+          v-model="currentPage"
+          :total-rows=7
+          :per-page="perPage"
+          aria-controls="flights-table"
+          @input="changePage"
+          ></b-pagination>
+        </b-col>
+        <b-col col lg ="2">
+          <b-form-select v-model="selected" :options="options"></b-form-select>         
+        </b-col>
+        <b-col col lg ="2">
+          <b-form-input v-model="filter"></b-form-input>
+        </b-col>
+        <b-col col lg ="1">
+          <b-button @click="filterFlights">Filter</b-button>
+        </b-col>
+      </b-row>    
+      <b-table id="flights-table" striped hover :items="flights"></b-table>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapActions, mapState } from 'vuex';
+export default {
+  name: 'Flights',
+  computed: {
+      ...mapState(['flights']),
+    },
+    data() {
+      return {
+        perPage: 3,
+        currentPage: 1,
+        filter: null,
+        selected: null,
+        options: [
+          { value: 'id', text: 'Flight Id' },
+          { value: 'airplaneId', text: 'Airplane Id' },
+          { value: 'startDestination', text: 'Start Destination' },
+          { value: 'endDestination', text: 'End Destination' },
+          { value: 'distance', text: 'Distance' },
+          { value: 'price', text: 'Price' },
+        ]
+      }
+    },
+  mounted: function() {
+     this.load_available_flights(this.currentPage);
+  },
+  methods: {
+    ...mapActions(['load_available_flights']),
+    ...mapActions(['load_filtered_flights']),
+    changePage: function() {
+      this.load_available_flights(this.currentPage);
+    },
+    filterFlights: function() {
+      console.log(this.selected);
+      console.log(this.filter);
+      var dto = [this.selected, this.filter];
+      this.load_filtered_flights(dto);
+    }
+  }
+}
+</script>
