350cf015eac956d1050a7b07793ee9794738cd43
gojkovicmatija99 <gojkovicmatija99@gmail.com>
2021-01-04 17:07:33 +0100

    Ostala jos opcija za brisanje aviona.

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,12 @@ const routes = [
   {
     path: '/flights',
     name: 'Flights',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Flights.vue')
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue')
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Flight from "../classes/Flight.js";
+import Airplane from "../classes/Airplane.js";
 
 Vue.use(Vuex)
-
-class Flight {
-  constructor(id, airplaneId, startDestination, endDestination, distance, price, currentPassengers, isCanceled) {
-    this.id = id;
-    this.airplaneId = airplaneId;
-    this.startDestination = startDestination;
-    this.endDestination = endDestination;
-    this.distance = distance;
-    this.price = price;
-    this.currentPassengers = currentPassengers;
-    if(isCanceled)
-      this._rowVariant = "danger" 
-  }
-}
 
 export default new Vuex.Store({
   state: {
@@ -33,7 +21,7 @@ export default new Vuex.Store({
         var price = flightsJson[index].price;
         var currentPassengers = flightsJson[index].currentPassengers;
         var canceled = flightsJson[index].canceled;
-        const flight = new Flight(id, airplaneId, startDestination, endDestination, distance, price, currentPassengers, canceled);
+        const flight = new Flight(id, airplaneId, startDestination, endDestination, distance, price, currentPassengers, canceled, false);
         state.flights.push(flight);
       }
     }
@@ -73,6 +61,76 @@ export default new Vuex.Store({
         return response.json()
       }).then((flightsJson) => {
         commit('set_flights', flightsJson)
+      }).catch((error) => {
+        if (typeof error.text === 'function')
+          error.text().then((errorMessage) => {
+            alert(errorMessage);
+          });
+        else
+          alert(error);
+      });
+    },
+    add_flight: function ({ commit }, flight) {
+      const json = JSON.stringify(flight)
+      fetch('http://localhost:8082/flight/add', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: json
+        }).then((response) => {
+        if (!response.ok)
+          throw response;
+        alert("Flight has been added!");
+      }).catch((error) => {
+        if (typeof error.text === 'function')
+          error.text().then((errorMessage) => {
+            alert(errorMessage);
+          });
+        else
+          alert(error);
+      });
+    },
+    cancel_flight: function ({ commit }, flightId) {
+      fetch('http://localhost:8082/flight/cancel/' + flightId, { method: 'get' }).then((response) => {
+        if (!response.ok)
+          throw response;
+        alert("Flight has been canceled!")
+      }).catch((error) => {
+        if (typeof error.text === 'function')
+          error.text().then((errorMessage) => {
+            alert(errorMessage);
+          });
+        else
+          alert(error);
+      });
+    },
+    add_airplane: function ({ commit }, airplane) {
+      const json = JSON.stringify(airplane)
+      fetch('http://localhost:8082/airplane/add', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: json
+        }).then((response) => {
+        if (!response.ok)
+          throw response;
+        alert("Airplane has been added!");
+      }).catch((error) => {
+        if (typeof error.text === 'function')
+          error.text().then((errorMessage) => {
+            alert(errorMessage);
+          });
+        else
+          alert(error);
+      });
+    },
+    remove_airplane: function ({ commit }, airplaneId) {
+      fetch('http://localhost:8082/airplane/delete/' + airplaneId, { method: 'get' }).then((response) => {
+        if (!response.ok)
+          throw response;
+        alert("Airplane has been removed!")
       }).catch((error) => {
         if (typeof error.text === 'function')
           error.text().then((errorMessage) => {
